6685f1daf8fbc4293ae86b11c574fea3a59406ba
Conor Bauer <cmbauer2@asu.edu>
2018-04-25 12:10:24 -0700

    start

--- A7/server.js
+++ A7/server.js
@@ -1,20 +1,77 @@
+var fs = require('fs');
+var AWS = require('aws-sdk');
+AWS.config.loadFromPath('./credentials.json');
+var s3 = new AWS.S3();
+
 var express = require("express");
 var app = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
+var port = 8080;
 
 var Client = require('node-rest-client').Client;
 
 var MS = require("mongoskin");
 var db = MS.db("mongodb://localhost:27017/ame470");
 
+app.use(methodOverride());
+app.use(require('connect').bodyParser());
+
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(express.static(__dirname + '/public'));
+app.use(errorHandler());
 
 app.get("/", function (req, res) {
     res.redirect("index.html");
 });
+
+app.post('/uploadImage', function(req, res){
+    var intname = req.body.fileInput;
+    var s3Path = '/' + intname;
+    var buf = new Buffer(req.body.data.replace(/^data:image\/\w+;base64,/, ""),'base64');
+    var params = {
+        Bucket:'ame4702018conorb',
+        ACL:'public-read',
+        Key:intname,
+        Body: buf,
+        ServerSideEncryption : 'AES256'
+    };
+    s3.putObject(params, function(err, data) {
+        console.log(err);
+        res.end("success");
+    });
+});
+
+app.post('/uploadFile', function(req, res){
+    var intname = req.body.fileInput;
+    var filename = req.files.input.name;
+    var fileType =  req.files.input.type;
+    var tmpPath = req.files.input.path;
+    var s3Path = '/' + intname;
+
+    fs.readFile(tmpPath, function (err, data) {
+        var params = {
+            Bucket:'ame4702018conorb',
+            ACL:'public-read',
+            Key:intname,
+            Body: data,
+            ServerSideEncryption : 'AES256'
+        };
+        s3.putObject(params, function(err, data) {
+            res.end("success");
+            console.log(err);
+        });
+    });
+  });
 
 app.get("/getImgData", function (req, res) {
   var data = req.query;
@@ -27,7 +84,6 @@ app.get("/getImgData", function (req, res) {
 
 app.get("/getAllImgs", function (req, res) {
   db.collection('img').find().sort({id:-1}).toArray(function(err, items) {
-    console.log(err, items);
     if(!items) items = [];
     res.send(items);
   });
@@ -59,9 +115,8 @@ app.get("/updateImg", function (req, res) {
   var data = req.query;
   var keys = Object.keys(data);
   var id = data.id;
- 
+
   db.collection("img").findOne({id:id}, function(err, result){
-      console.log(id, err, result);
       for(var i = 0; i < keys.length; i++){
         var key = keys[i];
         result[key] = data[key];
